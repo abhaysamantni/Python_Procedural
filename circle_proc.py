1f24f6faed3dc03fca450ed9f13bca5ef339d70f
abhaysamantni <abhay.samant@ni.com>
2022-09-12 16:17:13 -0500

    Added new test

--- circle_proc.py
+++ circle_proc.py
@@ -4,9 +4,9 @@ import math
 
 # The area function accepts a circle's radius as an
 # argument and returns the area and circumference of the circle.
-def area(radius, scalingFactor):
+def area(radius):
 
     #return math.pi * radius**2
-    return scalingFactor * math.pi * radius**2
+    return math.pi * radius**2
 
 
--- test circle_p.py
+++ test circle_p.py
@@ -1,4 +1,7 @@
-import circle_p
+import circle_proc
 
-A,C,d=circle_p.area(1.0)
-print(A,C, d)
+A=circle_proc.area(1.0)
+print(A)
+
+A,c,d=circle_proc.multiple_functions(1.0)
+print(A,c,d)
